5c7de50745df8437dc6471c47d52b9cccbba7c06
RamanMilashevich <roma.milashevich@gmail.com>
2024-08-17 22:05:33 +0200

    init:  end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let numString = n.toString();
+    let reversedStr = numString.split('').reverse().join('');
+    let reversedNumber = parseInt(reversedStr);
+    
+    return reversedNumber;
 }
